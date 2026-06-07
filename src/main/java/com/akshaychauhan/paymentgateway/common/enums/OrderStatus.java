c6d9ec617d5301d7df45e0977d72fd595bf12961
Akshay Chauhan <chauhanakshay395@gmail.com>
2026-06-07 12:33:59 +0530

    Added entities - Money, OrderStatus, OrderRecord

---
+++ src/main/java/com/akshaychauhan/paymentgateway/common/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.akshaychauhan.paymentgateway.common.enums;
+
+public enum OrderStatus {
+    CREATED,
+    ATTEMPTED,
+    PAID,
+    CANCELLED
+}
---
+++ src/main/java/com/akshaychauhan/paymentgateway/payment/entity/OrderRecord.java
@@ -0,0 +1,39 @@
+package com.akshaychauhan.paymentgateway.payment.entity;
+import com.akshaychauhan.paymentgateway.common.entity.Money;
+import com.akshaychauhan.paymentgateway.common.enums.OrderStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Table(name = "order_record")
+public class OrderRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // no FK - cross-service boundary
+    @Column(name = "merchant_id", nullable = false)
+    private UUID merchantId;
+
+    @Embedded
+    private Money money;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private OrderStatus orderStatus = OrderStatus.CREATED;
+
+    @Column(nullable = false)
+    private Integer attempts = 0;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private Map<String, Object> notes;
+
+    private LocalDateTime expiresAt;
+}
